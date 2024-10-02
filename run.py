e38a236d7dbd1b0cd9e99af41c7be8fcdecb071b
Miguel Filippo <miguel.calhabeu@usp.br>
2024-10-02 17:09:25 -0300

    first commit

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 import os
 
-os.system("mingw32\bin\gcc simple_simulator_Template.c -O3 -march=native -o simulador -Wall -lm")
-os.system("cls")
+gcc_path = os.path.join("mingw32", "bin", "gcc")
+
+warnings = os.popen(gcc_path + " simple_simulator_Template.c -O3 -march=native -o simulador -Wall -lm")
+
+if(warnings):
+    print("========= ALERTAS DE COMPILAÇÃO =========")
+    print()
+    print(warnings.read())
+    print("=========================================")
+    print()
+
 os.system("simulador")
